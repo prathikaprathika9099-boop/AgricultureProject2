e61e8594343a7cae106b12df5dbc999231ea7eda
prathikaprathika9099-boop <prathikaprathika9099@gmail.com>
2026-05-06 21:48:09 +0530

    Update OrderExcelExportService.java

--- src/main/java/com/example/AgriProject/service/OrderExcelExportService.java
+++ src/main/java/com/example/AgriProject/service/OrderExcelExportService.java
@@ -16,7 +16,7 @@ public class OrderExcelExportService {
 
     public byte[] exportOrdersToExcel(List<Order> orders) {
 
-        SXSSFWorkbook workbook = new SXSSFWorkbook(50); // keep only 50 rows in memory
+        Workbook workbook = new XSSFWorkbook(); // keep only 50 rows in memory
         workbook.setCompressTempFiles(true);
 
         try {
